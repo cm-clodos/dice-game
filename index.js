67145666402905a3284aa0a88b335958d9397a63
cm-clodos <claudiamartinez@claudias-macbook-air.home>
2021-04-07 10:07:25 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,70 @@
+"use strict";
+
+var randomNumber1 = Math.floor( (Math.random() * 6) + 1) ; //Zahl zwischen 1 und 6
+var randomNumber2 = Math.floor( (Math.random() * 6) + 1) ; //Zahl zwischen 1 und 6
+
+
+
+function player1 (randomNumber1) {
+    
+    if(randomNumber1 == 1){
+        document.querySelector(".img1").setAttribute("src","images/dice1.png");
+    }
+    else if (randomNumber1 == 2){
+        document.querySelector(".img1").setAttribute("src","images/dice2.png");
+    }
+    else if (randomNumber1 == 3){
+        document.querySelector(".img1").setAttribute("src","images/dice3.png");
+    }
+    else if (randomNumber1 == 4){
+        document.querySelector(".img1").setAttribute("src","images/dice4.png");
+    }
+    else if (randomNumber1 == 5){
+        document.querySelector(".img1").setAttribute("src","images/dice5.png");
+    }
+    else if (randomNumber1 == 6){
+        document.querySelector(".img1").setAttribute("src","images/dice6.png");
+    }
+
+}
+
+function player2 (randomNumber2) {
+    
+    if(randomNumber2 == 1){
+        document.querySelector(".img2").setAttribute("src","images/dice1.png");
+    }
+    else if (randomNumber2 == 2){
+        document.querySelector(".img2").setAttribute("src","images/dice2.png");
+    }
+    else if (randomNumber2 == 3){
+        document.querySelector(".img2").setAttribute("src","images/dice3.png");
+    }
+    else if (randomNumber2 == 4){
+        document.querySelector(".img2").setAttribute("src","images/dice4.png");
+    }
+    else if (randomNumber2 == 5){
+        document.querySelector(".img2").setAttribute("src","images/dice5.png");
+    }
+    else if (randomNumber2 == 6){
+        document.querySelector(".img2").setAttribute("src","images/dice6.png");
+    }
+
+}
+
+function vergleich() {
+    if(randomNumber1 > randomNumber2){
+        document.querySelector("h1").innerHTML = "Player 1 Wins!";
+        
+    }
+    else if (randomNumber1 < randomNumber2){
+        document.querySelector("h1").innerHTML = "Player 2 Wins!";
+    }
+    else if (randomNumber1 == randomNumber2){
+        document.querySelector("h1").innerHTML = "Draw!";
+    }
+    
+}
+player1(randomNumber1);
+player2(randomNumber2);
+vergleich();
+
